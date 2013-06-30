e3ddfdfede781ca83cd8397c3a3652aeb85fccb1
Hinnerk <elhinni@gmail.com>
2013-06-30 20:11:04 +0200

    remove unused resources

--- Jump_n_Bump/src/de/jumpnbump/usecases/game/graphics/PlayerDrawerFactory.java
+++ Jump_n_Bump/src/de/jumpnbump/usecases/game/graphics/PlayerDrawerFactory.java
@@ -41,21 +41,21 @@ public class PlayerDrawerFactory {
 		return Arrays.asList(convertedColor);
 	}
 
-	private static List<Bitmap> createListOfAllBitmaps(Resources resources) {
-		return Arrays.asList(
-				loadBitmap(resources, R.drawable.v4_pink_4steps_000001),
-				loadBitmap(resources, R.drawable.v4_pink_4steps_000002),
-				loadBitmap(resources, R.drawable.v4_pink_4steps_000003),
-				loadBitmap(resources, R.drawable.v4_pink_4steps_000004),
-				loadBitmap(resources, R.drawable.v4_pink_4steps_000005),
-				loadBitmap(resources, R.drawable.v4_pink_4steps_000006),
-				loadBitmap(resources, R.drawable.v4_pink_4steps_000007),
-				loadBitmap(resources, R.drawable.v4_pink_4steps_000008),
-				loadBitmap(resources, R.drawable.v4_pink_4steps_000009),
-				loadBitmap(resources, R.drawable.v4_pink_4steps_000010),
-				loadBitmap(resources, R.drawable.v4_pink_4steps_000011),
-				loadBitmap(resources, R.drawable.v4_pink_4steps_000012));
-	}
+	// private static List<Bitmap> createListOfAllBitmaps(Resources resources) {
+	// return Arrays.asList(
+	// loadBitmap(resources, R.drawable.v4_pink_4steps_000001),
+	// loadBitmap(resources, R.drawable.v4_pink_4steps_000002),
+	// loadBitmap(resources, R.drawable.v4_pink_4steps_000003),
+	// loadBitmap(resources, R.drawable.v4_pink_4steps_000004),
+	// loadBitmap(resources, R.drawable.v4_pink_4steps_000005),
+	// loadBitmap(resources, R.drawable.v4_pink_4steps_000006),
+	// loadBitmap(resources, R.drawable.v4_pink_4steps_000007),
+	// loadBitmap(resources, R.drawable.v4_pink_4steps_000008),
+	// loadBitmap(resources, R.drawable.v4_pink_4steps_000009),
+	// loadBitmap(resources, R.drawable.v4_pink_4steps_000010),
+	// loadBitmap(resources, R.drawable.v4_pink_4steps_000011),
+	// loadBitmap(resources, R.drawable.v4_pink_4steps_000012));
+	// }
 
 	private static Bitmap loadBitmap(Resources resources, int id) {
 		return BitmapFactory.decodeResource(resources, id);
