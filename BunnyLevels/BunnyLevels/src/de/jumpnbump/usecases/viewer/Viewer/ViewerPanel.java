e2cd541e0a21eb9f65d73d1aee8cf2a3b297bfa3
Hinnerk <elhinni@gmail.com>
2013-07-17 20:41:14 +0200

    storing xml files finished

--- BunnyLevels/BunnyLevels/src/de/jumpnbump/usecases/viewer/Viewer/ViewerPanel.java
+++ BunnyLevels/BunnyLevels/src/de/jumpnbump/usecases/viewer/Viewer/ViewerPanel.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.FileDialog;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
@@ -16,23 +17,27 @@ import javax.swing.JPanel;
 import de.jumpnbump.usecases.viewer.MyCanvas;
 import de.jumpnbump.usecases.viewer.xml.ObjectContainer;
 import de.jumpnbump.usecases.viewer.xml.XmlBuilder;
+import de.jumpnbump.usecases.viewer.xml.XmlStorer;
 
 public class ViewerPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	private MyCanvas myCanvas;
-	private XmlBuilder builder;
+	private final XmlBuilder builder;
 	private String lastFile;
+	private ObjectContainer model;
 
 	public ViewerPanel(String file) {
 		this.lastFile = file;
 
 		this.builder = new XmlBuilder();
+
 	}
 
 	public void build() {
 		setLayout(new BorderLayout());
-		this.myCanvas = new MyCanvas(parsexml());
+		parsexml();
+		this.myCanvas = new MyCanvas(this.model);
 		add(this.myCanvas, BorderLayout.CENTER);
 		addButtons();
 	}
@@ -42,7 +47,7 @@ public class ViewerPanel extends JPanel {
 		box.add(createLoadButton());
 		box.add(createRefreshButton());
 
-		box.add(new JButton("Save"));
+		box.add(createSaveButton());
 		add(box, BorderLayout.LINE_END);
 	}
 
@@ -72,8 +77,8 @@ public class ViewerPanel extends JPanel {
 	}
 
 	private void displayFile() {
-		ObjectContainer parsedxml = parsexml();
-		this.myCanvas.setObjectContainer(parsedxml);
+		parsexml();
+		this.myCanvas.setObjectContainer(this.model);
 		this.myCanvas.repaint();
 	}
 
@@ -84,10 +89,37 @@ public class ViewerPanel extends JPanel {
 		displayFile();
 	}
 
-	private ObjectContainer parsexml() {
+	private void parsexml() {
 		try {
-			return this.builder.parse(new FileInputStream(this.lastFile));
+			this.model = this.builder.parse(new FileInputStream(this.lastFile));
 		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private JButton createSaveButton() {
+		JButton button = new JButton("save");
+		button.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				save();
+			}
+		});
+		return button;
+	}
+
+	private void save() {
+		try {
+			ObjectContainer container = this.builder.parse(new FileInputStream(this.lastFile));
+			FileDialog dialog = new FileDialog((JFrame) ViewerPanel.this.getRootPane().getParent(), "save", FileDialog.SAVE);
+			dialog.setVisible(true);
+			this.lastFile = dialog.getDirectory() + File.separator + dialog.getFile();
+			java.io.File newFile = new java.io.File(this.lastFile);
+			newFile.delete();
+			newFile.createNewFile();
+			new XmlStorer(container).saveXml(newFile);
+		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
