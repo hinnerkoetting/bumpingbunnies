890545929c8694c4ce36e4f0af27b7ae560b7695
Hinnerk Oetting <hinnerk.oetting@gmail.com>
2014-08-03 19:59:15 +0200

    Added test for wall and water fixed object interaction

--- JnB_Test/test/de/oetting/bumpingbunnies/usecases/game/businesslogic/GameObjectInteractorTest.java
+++ JnB_Test/test/de/oetting/bumpingbunnies/usecases/game/businesslogic/GameObjectInteractorTest.java
@@ -1,23 +1,25 @@
 package de.oetting.bumpingbunnies.usecases.game.businesslogic;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 import java.util.Arrays;
 
+import org.apache.maven.model.Model;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
 import de.oetting.bumpingbunnies.usecases.game.ObjectProvider;
+import de.oetting.bumpingbunnies.usecases.game.dummy.sound.DummyMusicPlayer;
 import de.oetting.bumpingbunnies.usecases.game.factories.PlayerFactory;
 import de.oetting.bumpingbunnies.usecases.game.model.ModelConstants;
 import de.oetting.bumpingbunnies.usecases.game.model.Opponent;
 import de.oetting.bumpingbunnies.usecases.game.model.Player;
 import de.oetting.bumpingbunnies.usecases.game.model.Wall;
+import de.oetting.bumpingbunnies.usecases.game.model.Water;
 
 public class GameObjectInteractorTest {
 
@@ -35,17 +37,36 @@ public class GameObjectInteractorTest {
 		thenPlayerIsNotMovingAnymore();
 	}
 
-	private void whenCalculatingInteractions() {
-		GameObjectInteractor interactor = createInteractor();
-		interactor.interactWith(player);
+	@Test
+	public void interactWith_playerFallsIntoWater_reducesSpeedByOnePercent() {
+		givenPlayerMovesDown();
+		givenWaterIsBelowPlayer();
+		whenCalculatingInteractions();
+		thenPlayerMovingSlower();
+	}
+
+	private void givenPlayerMovesDown() {
+		createPlayerAtCenterOfWorld();
+		player.setMovementY(-100);
+	}
+
+	private void givenWaterIsBelowPlayer() {
+		Water water = new Water(-ModelConstants.PLAYER_WIDTH,
+				-ModelConstants.PLAYER_HEIGHT, ModelConstants.PLAYER_WIDTH,
+				-ModelConstants.PLAYER_HEIGHT / 2, new DummyMusicPlayer());
+		when(objectProvider.getAllWaters()).thenReturn(Arrays.asList(water));
 	}
 
 	private void givenPlayerMovesToRight() {
+		createPlayerAtCenterOfWorld();
+		player.setMovementX(1);
+	}
+
+	private void createPlayerAtCenterOfWorld() {
 		player = new PlayerFactory(1).createPlayer(-1, "test",
 				Opponent.createMyPlayer(""));
 		player.setCenterX(0);
 		player.setCenterY(0);
-		player.setMovementX(1);
 	}
 
 	private void givenWallIsRightOfPlayer() {
@@ -54,9 +75,18 @@ public class GameObjectInteractorTest {
 		when(objectProvider.getAllWalls()).thenReturn(Arrays.asList(wall));
 	}
 
+	private void whenCalculatingInteractions() {
+		GameObjectInteractor interactor = createInteractor();
+		interactor.interactWith(player);
+	}
+
 	private void thenPlayerIsNotMovingAnymore() {
 		assertThat(player.getState().getMovementX(), is(0));
 		assertThat(player.getState().getMovementY(), is(0));
+	}
+
+	private void thenPlayerMovingSlower() {
+		assertThat(player.getState().getMovementY(), is(-99));
 	}
 
 	private GameObjectInteractor createInteractor() {
---
+++ Jump_n_Bump/src/de/oetting/bumpingbunnies/usecases/game/dummy/sound/DummyMusicPlayer.java
@@ -0,0 +1,24 @@
+package de.oetting.bumpingbunnies.usecases.game.dummy.sound;
+
+import android.media.MediaPlayer.OnCompletionListener;
+import de.oetting.bumpingbunnies.usecases.game.sound.MusicPlayer;
+
+public class DummyMusicPlayer implements MusicPlayer {
+
+	@Override
+	public void start() {
+	}
+
+	@Override
+	public void pauseBackground() {
+	}
+
+	@Override
+	public void stopBackground() {
+	}
+
+	@Override
+	public void setOnCompletionListener(OnCompletionListener fixture) {
+	}
+
+}
