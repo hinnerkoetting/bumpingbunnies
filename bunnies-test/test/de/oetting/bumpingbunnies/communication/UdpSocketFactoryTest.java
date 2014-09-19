499218e7017c558c7e5fae2192afd8c763dd9e51
Hinnerk Oetting <hinnerk.oetting@gmail.com>
2014-09-19 10:38:23 +0200

    Fixed project configuration issue in test project

--- bunnies-test/test/de/oetting/bumpingbunnies/communication/UdpSocketFactoryTest.java
+++ bunnies-test/test/de/oetting/bumpingbunnies/communication/UdpSocketFactoryTest.java
@@ -25,24 +25,24 @@ import de.oetting.bumpingbunnies.usecases.game.model.Opponent;
 @Config(emulateSdk = 18)
 public class UdpSocketFactoryTest {
 
-    private UdpSocket socket1;
-    private UdpSocket socket2;
+	private UdpSocket socket1;
+	private UdpSocket socket2;
 
-    @Test
-    public void create_twoTimes_returnsSocketFromFirsttime() throws UnknownHostException, IOException {
-        Socket socket = mock(Socket.class);
-        when(socket.getInetAddress()).thenReturn(mock(InetAddress.class));
-        TCPSocket wlanSocket = new TCPSocket(socket, Opponent.createMyPlayer(""));
-        socket1 = UdpSocketFactory.singleton().create(wlanSocket, Opponent.createMyPlayer(""));
-        socket2 = UdpSocketFactory.singleton().create(wlanSocket, Opponent.createMyPlayer(""));
-        assertSame(socket1, socket2);
-    }
+	@Test
+	public void create_twoTimes_returnsSocketFromFirsttime() throws UnknownHostException, IOException {
+		Socket socket = mock(Socket.class);
+		when(socket.getInetAddress()).thenReturn(mock(InetAddress.class));
+		TCPSocket wlanSocket = new TCPSocket(socket, Opponent.createMyPlayer(""));
+		socket1 = UdpSocketFactory.singleton().create(wlanSocket, Opponent.createMyPlayer(""));
+		socket2 = UdpSocketFactory.singleton().create(wlanSocket, Opponent.createMyPlayer(""));
+		assertSame(socket1, socket2);
+	}
 
-    @After
-    public void tearDown() {
-        if (socket1 != null)
-            socket1.close();
-        if (socket2 != null)
-            socket2.close();
-    }
+	@After
+	public void tearDown() {
+		if (socket1 != null)
+			socket1.close();
+		if (socket2 != null)
+			socket2.close();
+	}
 }
