e228915021cdd0d212bb2aaaf9fe5a39321842ae
Hinnerk Oetting <hinnerk.oetting@gmail.com>
2014-09-27 14:28:34 +0200

    Fixed colors in android

--- bunnies-android/src/de/oetting/bumpingbunnies/usecases/game/graphics/PaintConverter.java
+++ bunnies-android/src/de/oetting/bumpingbunnies/usecases/game/graphics/PaintConverter.java
@@ -5,7 +5,8 @@ import android.graphics.Paint;
 public class PaintConverter {
 
 	public Paint convert(de.oetting.bumpingbunnies.core.graphics.Paint original) {
-		Paint paint = new Paint(original.getColor());
+		Paint paint = new Paint();
+		paint.setColor(original.getColor());
 		paint.setTextSize(original.getTextSize());
 		return paint;
 	}
