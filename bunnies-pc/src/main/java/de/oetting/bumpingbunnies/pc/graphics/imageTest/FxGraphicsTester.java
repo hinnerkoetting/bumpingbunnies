0db9231c9c54311e5e50c3bfaf647f133c1b3e88
Hinnerk Oetting <hinnerk.oetting+git@gmail.com>
2014-10-26 18:42:41 +0100

    Removed unused stuff

--- bunnies-pc/src/main/java/de/oetting/bumpingbunnies/pc/graphics/imageTest/FxGraphicsTester.java
+++ bunnies-pc/src/main/java/de/oetting/bumpingbunnies/pc/graphics/imageTest/FxGraphicsTester.java
@@ -3,15 +3,10 @@ package de.oetting.bumpingbunnies.pc.graphics.imageTest;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class FxGraphicsTester extends Application {
-
-	private ImageView imageView;
 
 	@Override
 	public void start(Stage stage) throws Exception {
@@ -20,14 +15,6 @@ public class FxGraphicsTester extends Application {
 		Scene scene = new Scene(root);
 		stage.setScene(scene);
 		stage.show();
-	}
-
-	private FlowPane createPane() {
-		FlowPane pane = new FlowPane();
-		Image image = new Image(getClass().getResourceAsStream("/drawable/v1d_down_1.png"));
-		imageView = new ImageView(image);
-		pane.getChildren().add(imageView);
-		return pane;
 	}
 
 	public static void main(String args[]) {
