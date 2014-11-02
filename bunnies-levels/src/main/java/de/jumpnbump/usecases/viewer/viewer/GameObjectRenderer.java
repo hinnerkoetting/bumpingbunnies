9f51972ca73aa3f40ee08aa762cee1ba56c091bd
Hinnerk Oetting <hinnerk.oetting+git@gmail.com>
2014-11-02 19:26:31 +0100

    Show tables in double notation

--- bunnies-levels/src/main/java/de/jumpnbump/usecases/viewer/viewer/GameObjectRenderer.java
+++ bunnies-levels/src/main/java/de/jumpnbump/usecases/viewer/viewer/GameObjectRenderer.java
@@ -7,6 +7,7 @@ import javax.swing.JList;
 import javax.swing.ListCellRenderer;
 
 import de.oetting.bumpingbunnies.model.game.objects.GameObject;
+import de.oetting.bumpingbunnies.model.game.objects.ModelConstants;
 
 public class GameObjectRenderer implements ListCellRenderer<GameObject> {
 
@@ -19,12 +20,19 @@ public class GameObjectRenderer implements ListCellRenderer<GameObject> {
 
 	@Override
 	public Component getListCellRendererComponent(JList<? extends GameObject> list, GameObject value, int index, boolean isSelected, boolean cellHasFocus) {
-		this.label.setText(value.id() + String.format(": [%d, %d] - [%d, %d]", value.minX(), value.minY(), value.maxX(), value.maxY()));
+		this.label
+				.setText(value.id()
+						+ String.format(": [%.2f, %.2f] - [%.2f, %.2f]", toFloat(value.minX()), toFloat(value.minY()), toFloat(value.maxX()),
+								toFloat(value.maxY())));
 		if (isSelected) {
 			this.label.setBackground(list.getSelectionBackground());
 		} else {
 			this.label.setBackground(list.getBackground());
 		}
 		return this.label;
+	}
+
+	private float toFloat(long value) {
+		return (float) ((double) value / ModelConstants.STANDARD_WORLD_SIZE);
 	}
 }
--- bunnies-levels/src/main/java/de/jumpnbump/usecases/viewer/viewer/SpawnpointRender.java
+++ bunnies-levels/src/main/java/de/jumpnbump/usecases/viewer/viewer/SpawnpointRender.java
@@ -6,6 +6,7 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.ListCellRenderer;
 
+import de.oetting.bumpingbunnies.model.game.objects.ModelConstants;
 import de.oetting.bumpingbunnies.model.game.objects.SpawnPoint;
 
 public class SpawnpointRender implements ListCellRenderer<SpawnPoint> {
@@ -19,12 +20,16 @@ public class SpawnpointRender implements ListCellRenderer<SpawnPoint> {
 
 	@Override
 	public Component getListCellRendererComponent(JList<? extends SpawnPoint> list, SpawnPoint value, int index, boolean isSelected, boolean cellHasFocus) {
-		this.label.setText(String.format("[%d, %d]", value.getX(), value.getY()));
+		this.label.setText(String.format("[%.2f, %.2f]", toFloat(value.getX()), toFloat(value.getY())));
 		if (isSelected) {
 			this.label.setBackground(list.getSelectionBackground());
 		} else {
 			this.label.setBackground(list.getBackground());
 		}
 		return this.label;
+	}
+
+	public float toFloat(long value) {
+		return (float) ((double) value / ModelConstants.STANDARD_WORLD_SIZE);
 	}
 }
