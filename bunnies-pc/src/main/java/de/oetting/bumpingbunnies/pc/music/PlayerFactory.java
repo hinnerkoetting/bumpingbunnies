66ce87d52bd98cd9c7378a2ed48b34a624fd75f5
Hinnerk Oetting <hinnerk.oetting@gmail.com>
2014-11-09 13:41:12 +0100

    can play more then one sound now

--- bunnies-pc/src/main/java/de/oetting/bumpingbunnies/pc/music/PlayerFactory.java
+++ bunnies-pc/src/main/java/de/oetting/bumpingbunnies/pc/music/PlayerFactory.java
@@ -4,23 +4,13 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 
-import javazoom.jl.decoder.JavaLayerException;
-import javazoom.jl.player.AudioDevice;
-import javazoom.jl.player.FactoryRegistry;
-
 public class PlayerFactory {
 
 	private final String classpath;
-	private AudioDevice audioDevice;
 	private byte[] cachedMp3;
 
 	public PlayerFactory(String classpath) {
 		this.classpath = classpath;
-		try {
-			audioDevice = FactoryRegistry.systemRegistry().createAudioDevice();
-		} catch (JavaLayerException e) {
-			throw new RuntimeException(e);
-		}
 	}
 
 	public Mp3Player createPlayer() {
@@ -28,7 +18,7 @@ public class PlayerFactory {
 			if (cachedMp3 == null) {
 				cachedMp3 = cacheMp3();
 			}
-			return new Mp3Player(new ByteArrayInputStream(cachedMp3), audioDevice);
+			return new Mp3Player(new ByteArrayInputStream(cachedMp3));
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
