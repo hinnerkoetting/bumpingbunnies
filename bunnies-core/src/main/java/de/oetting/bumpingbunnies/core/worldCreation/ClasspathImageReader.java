2af40556457d9a62886fb2f78f05625661cbdc96
Hinnerk Oetting <hinnerk.oetting@gmail.com>
2014-11-24 21:16:37 +0100

    show error when image does not exist

--- bunnies-core/src/main/java/de/oetting/bumpingbunnies/core/worldCreation/ClasspathImageReader.java
+++ bunnies-core/src/main/java/de/oetting/bumpingbunnies/core/worldCreation/ClasspathImageReader.java
@@ -2,13 +2,20 @@ package de.oetting.bumpingbunnies.core.worldCreation;
 
 import java.io.InputStream;
 
+import de.oetting.bumpingbunnies.logger.Logger;
+import de.oetting.bumpingbunnies.logger.LoggerFactory;
+
 public class ClasspathImageReader {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ClasspathImageReader.class);
 
 	public InputStream readAsStream(String fileName) {
 		String classpathName = "/drawable/" + fileName + addEndingIfNecessary(fileName);
 		InputStream resourceAsStream = getClass().getResourceAsStream(classpathName);
-		if (resourceAsStream == null)
-			throw new IllegalArgumentException("Could not find file in classpath: " + classpathName);
+		if (resourceAsStream == null) {
+			LOGGER.warn("Could not read " + fileName);
+			return getClass().getResourceAsStream("/drawable/error.png");
+		}
 		return resourceAsStream;
 	}
 
