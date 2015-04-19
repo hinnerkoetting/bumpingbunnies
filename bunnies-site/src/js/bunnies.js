a46a6a21b9461f52a242f550b1acc2a1d8efd66b
Hinnerk Oetting <hinnerk.oetting@gmail.com>
2015-04-19 10:17:50 +0200

    Added email address

---
+++ bunnies-site/src/js/bunnies.js
@@ -0,0 +1,7 @@
+//is this enough to avoid spam? :)
+function createMail() {
+	var part1 = 'bumpingbunnies';
+	var part2 = 'gmail';
+	var part3 = 'com';
+	return part1 + '@' + part2 + '.' + part3;
+}
