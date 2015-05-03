4fff23143eb3877a7f9fb5d2d85c93f95f9fbd7f
Hinnerk Oetting <hinnerk.oetting@consileon.de>
2015-05-03 17:43:06 +0200

    Refactoring: Removed unused classes

--- bunnies-core/src/main/java/de/oetting/bumpingbunnies/core/networking/receive/OpponentTypeFactory.java
+++
@@ -1,9 +0,0 @@
-package de.oetting.bumpingbunnies.core.networking.receive;
-
-import de.oetting.bumpingbunnies.core.networking.messaging.receiver.OpponentTypeReceiveFactory;
-
-public interface OpponentTypeFactory {
-
-	OpponentTypeReceiveFactory createReceiveFactory();
-
-}
