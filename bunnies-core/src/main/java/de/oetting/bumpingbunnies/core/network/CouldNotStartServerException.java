05eec2dd53f38744dd60d3d097dfc66accaa218d
Hinnerk Oetting <hinnerk.oetting@consileon.de>
2015-05-14 18:54:30 +0200

    Show error when it is not possible to make device visible for bluetooth

---
+++ bunnies-core/src/main/java/de/oetting/bumpingbunnies/core/network/CouldNotStartServerException.java
@@ -0,0 +1,10 @@
+package de.oetting.bumpingbunnies.core.network;
+
+public class CouldNotStartServerException extends RuntimeException {
+
+	public CouldNotStartServerException(Throwable cause) {
+		super(cause);
+	}
+
+	
+}
