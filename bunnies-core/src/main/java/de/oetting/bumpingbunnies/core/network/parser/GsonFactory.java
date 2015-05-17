9810f546363e49ce7ed460f4f20534796a081db5
Hinnerk Oetting <hinnerk.oetting@consileon.de>
2015-05-17 20:29:28 +0200

    Added custom format to player messages to save bandwidth

--- bunnies-core/src/main/java/de/oetting/bumpingbunnies/core/network/parser/GsonFactory.java
+++ bunnies-core/src/main/java/de/oetting/bumpingbunnies/core/network/parser/GsonFactory.java
@@ -4,12 +4,14 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import de.oetting.bumpingbunnies.model.game.objects.HorizontalMovementState;
+import de.oetting.bumpingbunnies.model.game.objects.PlayerState;
 
 public class GsonFactory {
 
 	public Gson create() {
 		GsonBuilder builder = new GsonBuilder();
 		builder.registerTypeAdapter(HorizontalMovementState.class, new HorizontalMovementStateTypeAdapter().nullSafe());
+		builder.registerTypeAdapter(PlayerState.class, new PlayerStateAdapter());
 		return builder.create();
 	}
 }
---
+++ bunnies-core/src/main/java/de/oetting/bumpingbunnies/core/network/parser/PlayerStateAdapter.java
@@ -0,0 +1,97 @@
+package de.oetting.bumpingbunnies.core.network.parser;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import de.oetting.bumpingbunnies.model.game.objects.HorizontalMovementState;
+import de.oetting.bumpingbunnies.model.game.objects.PlayerState;
+
+/**
+ * Custom adapter for the playerstate to optimize bandwith usage.
+ * This reduced the bandwith from ~4,5KB/s to 1,7KB/s (tested with networktester-project).
+ */
+public class PlayerStateAdapter extends TypeAdapter<PlayerState> {
+
+	@Override
+	public PlayerState read(JsonReader reader) throws IOException {
+		try {
+			String[] splitted = reader.nextString().split(separator());
+			PlayerState state = new PlayerState(decodeInt(splitted[0]));
+			state.setCenterX(decode(splitted[1]));
+			state.setCenterY(decode(splitted[2]));
+			state.setMovementX(decodeInt(splitted[3]));
+			state.setMovementY(decodeInt(splitted[4]));
+			state.setFacingLeft(decodeBoolean(splitted[5]));
+			state.setJumpingButtonPressed(decodeBoolean(splitted[6]));
+			state.setHorizontalMovementStatus(decodeMovement(splitted[7]));
+			return state;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private HorizontalMovementState decodeMovement(String nextString) {
+		return HorizontalMovementState.find(nextString);
+	}
+
+	private boolean decodeBoolean(String nextString) {
+		return nextString.equals("J");
+	}
+
+	private long decode(String nextString) {
+		return Long.parseLong(nextString, 16);
+	}
+
+	private int decodeInt(String nextString) {
+		return Integer.parseInt(nextString, 16);
+	}
+
+	private String decodeByte(String nextString) {
+		return new String(Base64.getDecoder().decode(nextString.getBytes()));
+	}
+
+	@Override
+	public void write(JsonWriter arg0, PlayerState arg1) throws IOException {
+		String createString = createString(arg1);
+		if(createString.contains("--")){
+			throw new IllegalArgumentException();
+		}
+		arg0.value(createString);
+	}
+
+	private String createString(PlayerState arg1) {
+		return encode(arg1.getId()) + separator() + //
+				encode(arg1.getCenterX()) + separator() + //
+				encode(arg1.getCenterY()) + separator() + //
+				encode(arg1.getMovementX()) + separator() + //
+				encode(arg1.getMovementY()) + separator() + //
+				encode(arg1.isFacingLeft()) + separator() + //
+				encode(arg1.isJumpingButtonPressed()) + separator() + //
+				encode(arg1.getHorizontalMovementStatus());
+	}
+
+	private String separator() {
+		return ",";
+	}
+
+	private String encode(long value) {
+		return Long.toString(value, 16);
+	}
+
+	private String encode(int value) {
+		return Integer.toString(value, 16);
+	}
+
+	private String encode(boolean value) {
+		return value ? "J" : "N";
+	}
+
+	private String encode(HorizontalMovementState value) {
+		return value.getEncoding();
+	}
+
+}
