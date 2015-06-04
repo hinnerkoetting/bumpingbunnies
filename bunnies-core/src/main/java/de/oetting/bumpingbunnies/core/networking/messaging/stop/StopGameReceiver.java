5ca0e7beb1da2ad2759344ff33b6eca84ac26d11
Hinnerk Oetting <hinnerk.oetting@gmail.com>
2015-06-04 22:28:23 +0200

    Fixed exception when a player ended the game

--- bunnies-core/src/main/java/de/oetting/bumpingbunnies/core/networking/messaging/stop/StopGameReceiver.java
+++ bunnies-core/src/main/java/de/oetting/bumpingbunnies/core/networking/messaging/stop/StopGameReceiver.java
@@ -4,17 +4,17 @@ import de.oetting.bumpingbunnies.core.network.MessageReceiverTemplate;
 import de.oetting.bumpingbunnies.core.network.NetworkToGameDispatcher;
 import de.oetting.bumpingbunnies.model.network.MessageId;
 
-public class StopGameReceiver extends MessageReceiverTemplate<Void> {
+public class StopGameReceiver extends MessageReceiverTemplate<String> {
 
 	private final GameStopper gameStopper;
 
 	public StopGameReceiver(NetworkToGameDispatcher dispatcher, GameStopper gameStopper) {
-		super(dispatcher, MessageId.STOP_GAME, Void.class);
+		super(dispatcher, MessageId.STOP_GAME, String.class);
 		this.gameStopper = gameStopper;
 	}
 
 	@Override
-	public void onReceiveMessage(Void object) {
+	public void onReceiveMessage(String object) {
 		gameStopper.gameStopped();
 	}
 
