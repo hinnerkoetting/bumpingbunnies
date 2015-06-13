0b8113b20cc2a89163d4decf5760083c86776d2a
Hinnerk Oetting <hinnerk.oetting@consileon.de>
2015-06-13 17:44:22 +0200

    Fixed player images cache when image size changes

--- bunnies-core/src/main/java/de/oetting/bumpingbunnies/core/game/graphics/factory/PlayerImagesCache.java
+++ bunnies-core/src/main/java/de/oetting/bumpingbunnies/core/game/graphics/factory/PlayerImagesCache.java
@@ -7,6 +7,8 @@ import de.oetting.bumpingbunnies.model.game.objects.ImageWrapper;
 public class PlayerImagesCache implements PlayerImagesProvider {
 
 	private final PlayerImagesProvider provider;
+	private int lastWidth;
+	private int lastheight;
 	
 	private List<ImageWrapper> runningImages;
 	private List<ImageWrapper> fallingImages;
@@ -19,30 +21,47 @@ public class PlayerImagesCache implements PlayerImagesProvider {
 	}
 
 	public synchronized List<ImageWrapper> loadAllRunningImages(int width, int heigth) {
+		storeWidthAndCheckCache(width, heigth);
 		if (runningImages == null) 
 			runningImages = provider.loadAllRunningImages(width, heigth);
 		return runningImages;
 	}
 
+	private void storeWidthAndCheckCache(int width, int heigth) {
+		if (width != lastWidth || heigth != lastheight) {
+			runningImages = null;
+			fallingImages = null;
+			jumpingImages = null;
+			sittingImages = null;
+			jumpingUpImages = null;
+			lastheight = heigth;
+			lastWidth = width;
+		}
+	}
+
 	public synchronized List<ImageWrapper> loadAllFallingImages(int width, int heigth) {
+		storeWidthAndCheckCache(width, heigth);
 		if (fallingImages == null)
 			fallingImages = provider.loadAllFallingImages(width, heigth);
 		return fallingImages;
 	}
 
 	public synchronized List<ImageWrapper> loadAllJumpingImages(int width, int heigth) {
+		storeWidthAndCheckCache(width, heigth);
 		if (jumpingImages == null)
 			jumpingImages = provider.loadAllJumpingImages(width, heigth);
 		return jumpingImages;
 	}
 
 	public synchronized List<ImageWrapper> loadAllSittingImages(int width, int heigth) {
+		storeWidthAndCheckCache(width, heigth);
 		if (sittingImages == null)
 			sittingImages = provider.loadAllSittingImages(width, heigth);
 		return sittingImages;
 	}
 
 	public synchronized List<ImageWrapper> loadAllJumpingUpImages(int width, int heigth) {
+		storeWidthAndCheckCache(width, heigth);
 		if (jumpingUpImages == null)
 			jumpingUpImages = provider.loadAllJumpingUpImages(width, heigth);
 		return jumpingUpImages;
