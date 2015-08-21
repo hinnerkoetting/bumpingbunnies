c2d6f9dce43d6bf0fea58920a3353dabb52ce4c3
Hinnerk Oetting <hinnerk.oetting@consileon.de>
2015-08-21 18:07:24 +0200

    Can now add circles and rectangles

--- bunnies-editor-html/bunnies.js
+++ bunnies-editor-html/bunnies.js
@@ -3,14 +3,45 @@
 var startX;
 var startY;
 
+var currentEditMode;
+
+function setRectangleEditingMode() {
+  currentEditMode = {
+    element: "rect",
+    init: function(event) {
+        d3.select(".editedObject").
+          attr('x', event.offsetX).
+          attr('y', event.offsetY);
+    },
+    movementListeners: [
+      adjustWidth, adjustHeight
+    ]
+  };
+}
+
+function setCircleEditingMode() {
+  currentEditMode = {
+    element: "circle",
+    init: function(event) {
+      d3.select(".editedObject").
+        attr('cx', event.offsetX).
+        attr('cy', event.offsetY);
+    },
+    movementListeners: [
+      adjustRadius
+    ]
+  };
+}
+
+// setRectangleEditingMode();
+setCircleEditingMode();
+
+
 function createObject(event) {
   d3.select("#editorCanvas")
-    .append("rect")
-    .attr('class', 'editedObject')
-    .attr('x', event.offsetX)
-    .attr('y', event.offsetY)
-    .attr('width', 5)
-    .attr('height', 5);
+    .append(currentEditMode.element)
+    .attr('class', 'editedObject');
+  currentEditMode.init(event);
 }
 
 function onDragCanvas(event) {
@@ -22,8 +53,10 @@ function onDragCanvas(event) {
     startY = event.offsetY;
   }
   editedObjects.each(function (object) {
-      adjustWidth($(this), event);
-      adjustHeight($(this), event);
+    var editedObject = $(this);
+    currentEditMode.movementListeners.forEach(function(listener) {
+        listener(editedObject, event);
+    });
   });
 
 }
@@ -46,6 +79,12 @@ function adjustHeight(editedObject, event) {
   }
 }
 
+function adjustRadius(editedObject, event) {
+  var diffX = Math.pow(event.offsetX - startX, 2);
+  var diffY = Math.pow(event.offsetY - startY, 2);
+  editedObject.attr('r', Math.sqrt(diffX + diffY));
+}
+
 function onMouseMoveCanvas(event) {
   if (event.buttons == 0) {
     d3.select(".editedObject").attr('class', '');
@@ -61,8 +100,8 @@ function getCanvas() {
 
 
 $( document ).ready(function() {
-            	 var canvas = document.getElementById("editorCanvas");
-            	 canvas.addEventListener('mousemove', function(event) {
-                	 onMouseMoveCanvas(event);
-                	 });
-            });
+	 var canvas = document.getElementById("editorCanvas");
+	 canvas.addEventListener('mousemove', function(event) {
+    	 onMouseMoveCanvas(event);
+    	 });
+});
